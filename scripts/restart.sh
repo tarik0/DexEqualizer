4714b6c4ea8610704c52a5c832639c210dcf7f98
tarik0 <tarik0@github.com>
2023-02-04 01:36:08 +0300

    Added hot tokens monitor system.

--- scripts/restart.sh
+++ scripts/restart.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-var="$(pm2 list | grep dexeq_out | grep "running" | wc -c)"
+var="$(pm2 list | grep dexeq_out | grep "stopped" | wc -c)"
 
 if [[ $var =~ "0" ]]; then
     pm2 stop dexeq_out && pm2 start dexeq_out
